24a361e67a3427953d1a6e1c48f4b62436135ab0
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-09-12 16:58:47 +0300

    Camera Target Bounds

--- lib/view/screen/google_map_view.dart
+++ lib/view/screen/google_map_view.dart
@@ -11,25 +11,34 @@ class GoogleMapView extends StatefulWidget {
 
 class _GoogleMapViewState extends State<GoogleMapView> {
   late CameraPosition initialCameraPosition;
+
   @override
   @override
   void initState() {
-    initialCameraPosition= const CameraPosition(
-        //required
-        target: LatLng(29.955404, 32.476655),
-        //World View 0 -> 3
-        //Country View 4 -> 6
-        //City View 10 -> 12
-        //Street View 13 -> 17
-        //Building View 18 -> 20
-        zoom: 18);
+    initialCameraPosition = const CameraPosition(
+      //required
+      target: LatLng(29.955404, 32.476655),
+      //World View 0 -> 3
+      //Country View 4 -> 6
+      //City View 10 -> 12
+      //Street View 13 -> 17
+      //Building View 18 -> 20
+      zoom: 16,
+
+    );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: GoogleMap(initialCameraPosition: initialCameraPosition),
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: initialCameraPosition,
+        //بتحدد النطاق الي عاوزه اعرضه في ماب بس
+        cameraTargetBounds: CameraTargetBounds(LatLngBounds(
+            southwest: const LatLng(29.936142, 32.476483),
+            northeast:const LatLng(29.970306, 32.522784))),
+      ),
     );
   }
 }
