316168330a6867575127fb328bed4950721d3e24
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-09-12 17:10:37 +0300

    Camera Update

--- lib/view/screen/google_map_view.dart
+++ lib/view/screen/google_map_view.dart
@@ -11,7 +11,7 @@ class GoogleMapView extends StatefulWidget {
 
 class _GoogleMapViewState extends State<GoogleMapView> {
   late CameraPosition initialCameraPosition;
-
+  late GoogleMapController googleMapController;
   @override
   @override
   void initState() {
@@ -32,12 +32,35 @@ class _GoogleMapViewState extends State<GoogleMapView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: initialCameraPosition,
-        //بتحدد النطاق الي عاوزه اعرضه في ماب بس
-        cameraTargetBounds: CameraTargetBounds(LatLngBounds(
-            southwest: const LatLng(29.936142, 32.476483),
-            northeast:const LatLng(29.970306, 32.522784))),
+      body: Stack(
+        children: [
+          GoogleMap(
+            //ده بيبقي ثابت مش بيتغير و بيشتغل اول ما افتح ماب
+            initialCameraPosition: initialCameraPosition,
+            //لو محتاجه اعمل ابديت ع ماب
+            onMapCreated: (  GoogleMapController controller){
+              googleMapController=controller;
+            },
+            //بتحدد النطاق الي عاوزه اعرضه في ماب بس
+            // cameraTargetBounds: CameraTargetBounds(LatLngBounds(
+            //     southwest: const LatLng(29.936142, 32.476483),
+            //     northeast:const LatLng(29.970306, 32.522784))),
+
+          ),
+          Positioned(
+            bottom: 16,
+              left: 16,
+              right: 16,
+              child: ElevatedButton(onPressed: (){
+                CameraPosition   newLocation = const CameraPosition(
+                  target: LatLng(29.936142, 32.476483),
+                  zoom: 16,
+
+                );
+
+                googleMapController.animateCamera(CameraUpdate.newCameraPosition(newLocation));
+              }, child: const Text("Change Location")))
+        ],
       ),
     );
   }
